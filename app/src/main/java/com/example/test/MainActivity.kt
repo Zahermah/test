754d8dfe194b130f4bddb933ce4cb3887cd98408
Zaher Mahmoudi <zahermah@live.se>
2023-11-28 03:25:45 +0100

    removed 2023

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Github2023")
+                    Greeting("Github")
                 }
             }
         }
